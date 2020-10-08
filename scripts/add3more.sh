7ff9040ea9ca2c8f43f1e07a9941a3f5a894369f
Vicky Staubly <vicky@steeds.com>
2020-10-08 00:32:28 -0400

    Fixed test shell scripts so they can run on real Linux vs Cygwin

--- scripts/add3more.sh
+++ scripts/add3more.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+
 curl -s -X POST -H "Content-Type: application/json" 'http://127.0.0.1:8080/' --data '{ "name": "Flight 4", "type": "Passenger", "size": "Small" }'
 curl -s -X POST -H "Content-Type: application/json" 'http://127.0.0.1:8080/' --data '{ "name": "Flight 5", "type": "Passenger", "size": "Large" }'
 curl -s -X POST -H "Content-Type: application/json" 'http://127.0.0.1:8080/' --data '{ "name": "Flight 6", "type": "Emergency", "size": "Large" }'
+echo " "
--- scripts/dequeue.sh
+++ scripts/dequeue.sh
@@ -1,1 +1,4 @@
+#!/bin/sh
+
 curl -s -X DELETE 'http://127.0.0.1:8080/'
+echo " "
